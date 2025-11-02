4ca1a86d245c2b0983db87b57c4e41111c5017f5
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-02 23:57:24 +0100

    added the main game file

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def main():
     game_over = False
 
     while not game_over:
-        Board.display_board(Board)
+        Board.display_board(board)
         Board.player_move(current_player, Board)
-        if Board.check_winner(Board) == True:
-            Board.display_board(Board)
+        if Board.check_winner(board) == True:
+            Board.display_board(board)
             print(f"The winner is {current_player}")
             game_over = True
-        if Board.is_draw(Board):
-            Board.display_board(Board)
+        if Board.is_draw(board):
+            Board.display_board(board)
             print("It's a draw !")
             game_over = True
         current_player = Board.switch_player(current_player)
